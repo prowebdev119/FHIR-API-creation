bb3a489d54546b1bfbbbfe2c23121732c0e2a8ad
prowebdev110 <tieosong119@gmail.com>
2022-11-01 09:01:34 +0900

    Update Fhir.js

--- models/Fhir.js
+++ models/Fhir.js
@@ -2,6 +2,12 @@ const mongoose = require("mongoose")
 const Schema = mongoose.Schema
 
 const FhirSchema = new Schema({
+    resourceType:String,
+    id:String,
+    identifier:Object,
+    timestamp:String,
+    type:String,
+    entry:[Object]  
 })
 
 module.exports = mongoose.model("Fhir",FhirSchema)
