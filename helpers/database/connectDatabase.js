966b5d2666dc9e75b98d27e6a4fca1dbdaf9c2a8
prowebdev110 <tieosong119@gmail.com>
2022-11-01 09:00:30 +0900

    Update connectDatabase.js

--- helpers/database/connectDatabase.js
+++ helpers/database/connectDatabase.js
@@ -1,12 +1,6 @@
 const mongoose = require("mongoose");
 
 const connectDatabase = () => {
-  mongoose
-    .connect(process.env.MONGO_URI, { useNewUrlParser: true })
-    .then(() => {
-      console.log("MongoDb Connection Successful");
-    })
-    .catch(err => console.log(new Error(err)));
 };
 
 module.exports = connectDatabase;
