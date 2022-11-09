1d142ebe4f4c2080a0467b362186b36d892542e8
prowebdev110 <tieosong119@gmail.com>
2022-11-10 01:35:19 +0900

    Update importFiles.js

---
+++ helpers/file/importFiles.js
@@ -0,0 +1,9 @@
+const fs = require("fs")
+const path = require("path")
+
+const importFiles = () => {
+    let data = fs.readdirSync("../../data")
+    console.log(data)
+}
+
+module.exports = importFiles
